9c966eff223cb666e4e0a512a75e5e99950756a0
Alex <beccialex2002@gmail.com>
2023-05-24 15:19:13 -0300

    Footer remake

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,71 +1,86 @@
 import React from "react";
-import {GrGithub} from "react-icons/gr"
+import { GrGithub } from "react-icons/gr";
 function Footer() {
   return (
-    <footer className="text-gray-400 bg-white body-font">
-      <div className="container py-1 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
-        <div className=" bg-opacity-75 border-2">
-          <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-            <p className="text-gray-400 text-sm text-center sm:text-left">
-              © 2023 Tecno-Gaming —
-              <a
-                href="https://twitter.com/knyttneve"
-                rel="noopener noreferrer"
-                className="text-gray-500 ml-1"
-                target="_blank"
+    <footer className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col"></div>
+      <div className="bg-gray-100">
+        <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+          <p classNameName="text-gray-400 text-sm text-center sm:text-left">
+            © 2023 Tecno-Gaming —
+            <a
+              href="https://twitter.com/knyttneve"
+              rel="noopener noreferrer"
+              classNameName="text-gray-500 ml-1"
+              target="_blank"
+            >
+              Becci Alex
+            </a>
+          </p>
+          <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+            <a
+              href="https://github.com/AlexBecci"
+              target="_blank"
+              classNameName="ml-3 text-gray-400"
+            >
+              <GrGithub className="h-6 w-6 mb-1" />
+            </a>
+            <a
+              href="https://twitter.com/besisikun"
+              target="_blank"
+              className="ml-3 text-gray-500"
+            >
+              <svg
+                fill="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                className="w-5 h-5"
+                viewBox="0 0 24 24"
               >
-                Becci Alex
-              </a>
-            </p>
-            <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-              <a href="https://twitter.com/besisikun" target="_blank" className="ml-3 text-gray-400">
-                <svg
-                  fill="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                </svg>
-              </a>
-              <a href="https://www.instagram.com/becci.ale/" target="_blank" className="ml-3 text-gray-400">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                  <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                </svg>
-              </a>
-              <a href="https://www.linkedin.com/in/alex-becci-82551a22b/" target="_blank" className="ml-3 text-gray-400">
-                <svg
-                  fill="currentColor"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="0"
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="none"
-                    d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-                  ></path>
-                  <circle cx="4" cy="4" r="2" stroke="none"></circle>
-                </svg>
-              </a>
-              <a href="https://github.com/AlexBecci" target="_blank" className="ml-3 text-gray-400">
-                <GrGithub className="h-6 w-6"/>
-              </a>
-            </span>
-          </div>
+                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+              </svg>
+            </a>
+            <a
+              href="https://www.instagram.com/becci.ale/"
+              target="_blank"
+              className="ml-3 text-gray-500"
+            >
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                className="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+              </svg>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/alex-becci-82551a22b/"
+              target="_blank"
+              className="ml-3 text-gray-500"
+            >
+              <svg
+                fill="currentColor"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="0"
+                className="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="none"
+                  d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                ></path>
+                <circle cx="4" cy="4" r="2" stroke="none"></circle>
+              </svg>
+            </a>
+          </span>
         </div>
       </div>
     </footer>
